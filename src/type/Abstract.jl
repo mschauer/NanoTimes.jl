6681c703913303c98e4378e14cdeee93a14e21b9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-15 03:17:31 -0400

    Create Abstract.jl

---
+++ src/type/Abstract.jl
@@ -0,0 +1,4 @@
+abstract type AbstractDateTime <: Base.Dates.AbstractTime end
+abstract type AbstractTimeSpan <: Base.Dates.AbstractTime end
+abstract type AbstractNanoTime <: AbstractDateTime end
+abstract type AbstractNanoSpan <: AbstractTimeSpan end
