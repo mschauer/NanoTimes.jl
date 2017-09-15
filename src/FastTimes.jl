f3c807a1d2649da983789448d3c86ad1943f790c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-15 02:55:04 -0400

    Update FastTimes.jl

--- src/FastTimes.jl
+++ src/FastTimes.jl
@@ -7,6 +7,8 @@ import Base.Dates: datetime2rata, rata2datetime,
     datetime2unix, unix2datetime,
     date2epochdays, epochdays2date
 
+const IntTime = Int64
+
 include "support/granular_consts.jl"
 include "support/rata_conversion.jl"
 
