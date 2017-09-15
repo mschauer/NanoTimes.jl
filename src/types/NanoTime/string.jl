c5628550b7d2d9d96068f083796458137daefc23
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-15 07:34:32 -0400

    Update string.jl

--- src/types/NanoTime/string.jl
+++ src/types/NanoTime/string.jl
@@ -1,11 +1,3 @@
-const POSITIVE = '+'
-const NEGATIVE = '−'
-const HMS_SEP  = ':'
-const YMD_SEP  = '-'
-const DTM_SEP  = 'T'
-const UT_SFX   = 'Ƶ'
-const UTC_SFX  = 'Z'
-
 function Base.string(x::NanoTime{T}) where T
     dt = Date(x)
     tm = Time(x)
@@ -21,8 +13,7 @@ end
 function Base.string(x::NanoTime{T}, lcl2ut::Second) where T
     dt = Date(x)
     tm = Time(x)
-    mins, secs = divrem(abs(lcl2ut.value), SECONDS_PER_MINUTE)
-    hrs, mins  = fldmod(mins, MINUTES_PER_HOUR)
-    offset = string( (signbit(lcl2ut) ? NEGATIVE : POSITIVE), hrs, COLON, mns )
+    mins = div(lcl2ut.value, SECONDS_PER_MINUTE)
+    offset = signedhm(mins)
     return string(dt, "T", tm, offset)
 end
