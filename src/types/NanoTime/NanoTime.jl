b2c023ac650c127a37a725a9ace97990e03affad
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 01:56:16 -0400

    Update NanoTime.jl

--- src/types/NanoTime/NanoTime.jl
+++ src/types/NanoTime/NanoTime.jl
@@ -3,13 +3,3 @@ struct NanoTime{T} <: AbstractNanoTime
 end
 
 @inline value(x::NanoTime{T}) where T<:IntTimes = x.value
-
-Base.promote_type(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int128}
-Base.promote_rule(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int128}
-Base.convert(::Type{NanoTime{Int128}}, x::NanoTime{Int64}) = NanoTime{Int128}(value(x))
-function Base.convert(::Type{NanoTime{Int64}}, x::NanoTime{Int128})
-    if value(x) < typemin(Int64) || value(x) > typemax(Int64)
-       throw(InexactError("x out of bounds $(typemin(Int64)) <= $(x) <= $(typemax(Int64))"))
-    end
-    return NanoTime{Int64}(value(x))
-end
