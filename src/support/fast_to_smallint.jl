fa92eb80405b05b9679fdf7704df40441438c389
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 15:21:00 -0400

    Update fast_to_smallint.jl

--- src/support/fast_to_smallint.jl
+++ src/support/fast_to_smallint.jl
@@ -6,13 +6,13 @@ function gen_placevalue_dict()
             place1 = UInt8(place1s)
             key = (place10 << 4) | place1
             value = place10 * 0x0a + place1
-            placevalue[ key ] = value
+            placevalue[ key ] = Int16(value)
          end
     end
     return placevalue
 end
 
-const Place1s    = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const Place1s    = Int16[0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09]
 const PlaceValue = gen_placevalue_dict()
 
 @inline placevalue_key(place10::UInt8, place1::UInt8) = place10 << 4 | place1
@@ -24,40 +24,48 @@ const Offset0 = UInt8('0')
 
 @inline two_chars_to_uints(place10::Char, place1::Char) =
     char_to_uint(place10), char_to_uint(place1)
-@inline str2_to_uints(str::String) = @inbounds two_chars_to_uints(str[1], str[2])
+@inline str2_to_uints(str::String) = two_chars_to_uints(str[1], str[2])
 
 @inline two_chars_to_key(place10::Char, place1::Char) =
     placevalue_key(two_chars_to_uints(place10, place1)...)
 
-function str1_to_int(str::String) 
-    @inbounds return Place1s[ char_to_uint(str[1]) ]
+@inline function str1_to_int(str::String)
+    @inbounds return
+	Place1s[ char_to_value(str[1]) ]
 end
 
-function str2_to_int(str::String)
-    @inbounds return PlaceValue[ two_chars_to_key(str[1], str[2]) ]
+@inline str2_to_int(str::String)
+    @inbounds return
+	PlaceValue[ two_chars_to_key(str[1], str[2]) ]
 end
 
-function str3_to_int(str::String)
-    @inbounds return 100 * PlaceValue[ two_chars_to_key('0', str[1]) ] +
-                           PlaceValue[ two_chars_to_key(str[2], str[3]) ]
+@inline str3_to_int(str::String)
+    @inbounds return
+    100 * PlaceValue[ two_chars_to_key('0', str[1]) ] +
+          PlaceValue[ two_chars_to_key(str[2], str[3]) ]
 end
 
-function str4_to_int(str::String)
-    @inbounds return 100 * PlaceValue[ two_chars_to_key(str[1], str[2]) ] +
-                           PlaceValue[ two_chars_to_key(str[3], str[4]) ]
+@inline function str4_to_int(str::String)
+    @inbounds return
+	100 * PlaceValue[ two_chars_to_key(str[1], str[2]) ] +
+          PlaceValue[ two_chars_to_key(str[3], str[4]) ]
 end
 
-function str_to_int(str::String)
-    n = min(4,length(str))
+function str_to_int(str::String, targetType::Integer=Int)
+    n = length(str)
     result = 0
-    if n == 2
-        result = str2_to_int(str)
-    elseif n == 3
-        result = str3_to_int(str)
-    elseif n == 4
-        result = str4_to_int(str)
-    elseif n == 1
-        result = str1_to_int(str)
-    end
-    return result
+    if iseven(n)
+        if n == 2
+            result = str2_to_int(str)
+		elseif n == 4
+		    result = str4_to_int(str)
+		end
+	else
+        if n == 3
+            result = str3_to_int(str)
+        elseif n == 1
+            result = str1_to_int(str)
+        end
+	end
+	return targetType(result)
 end
