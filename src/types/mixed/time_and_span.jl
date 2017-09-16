c371d32c097701aa7c25ac013ad338154ffdeda6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 02:34:14 -0400

    Update time_and_span.jl

--- src/types/mixed/time_and_span.jl
+++ src/types/mixed/time_and_span.jl
@@ -1,3 +1,5 @@
+import Base: convert, promote_rule, promote_type, promote
+
 Base.promote_type(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int128}
 Base.promote_rule(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int128}
 Base.convert(::Type{NanoTime{Int128}}, x::NanoTime{Int64}) = NanoTime{Int128}(value(x))
@@ -12,13 +14,15 @@ Base.promote_type(::Type{NanoSpan{Int64}}, ::Type{NanoSpan{Int128}}) = NanoSpan{
 Base.promote_rule(::Type{NanoSpan{Int64}}, ::Type{NanoSpan{Int128}}) = NanoSpan{Int128}
 
 NanoSpan{Int128}(x::NanoSpan{Int64}) = NanoSpan{Int128}(value(x))
-#Base.convert(::Type{NanoSpan{Int128}}, x::NanoSpan{Int64}) = NanoSpan{Int128}(value(x))
+convert(::Type{NanoSpan{Int128}}, x::NanoSpan{Int64}) = NanoSpan{Int128}(value(x))
+
 function Base.convert(::Type{NanoSpan{Int64}}, x::NanoSpan{Int128})
     if value(x) < typemin(Int64) || value(x) > typemax(Int64)
        throw(InexactError("x out of bounds $(typemin(Int64)) <= $(x) <= $(typemax(Int64))"))
     end
     return NanoSpan{Int64}(value(x))
 end
+NanoSpan{Int64}(x::NanoSpan{Int128}) = convert(NanoSpan{Int64}, x)
 
 Base.promote(x::NanoTime{Int64},  y::NanoSpan{Int128}) = (NanoTime{Int128}(x), y)
 Base.promote(x::NanoTime{Int128}, y::NanoSpan{Int64})  = (x, NanoSpan{Int128}(y))
