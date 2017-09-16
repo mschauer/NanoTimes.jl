e7c2cb6c9b105b119d49195627a46d9b348579d8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 02:19:42 -0400

    Update arith.jl

--- src/types/NanoTime/arith.jl
+++ src/types/NanoTime/arith.jl
@@ -1,1 +1,16 @@
+import Base: (+), (-)
 
+function (-)(x::NanoTime{I}, y::NanoTime{I}) where I<:IntTimes
+    nanos = nanoseconds(x) - nanoseconds(y)
+    return NanoSpan(nanos)
+end
+
+(-)(x::NanoTime{I1}, y::NanoTime{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
+
+for P in (:Nanosecond, :Microsecond, :Millisecond, :Second, :Minute, :Hour, :Day, :Week)
+    @eval begin
+        (+)(x::NanoTime{I}, p::$P) where I<:IntTypes = NanoTime{I}(nanosecs(x) + nanosecs(p))
+        (+)(p::$P, x::NanoTime{I}) where I<:IntTypes = NanoTime{I}(nanosecs(x) + nanosecs(p))
+        (-)(x::NanoTime{I}, p::$P) where I<:IntTypes = NanoTime{I}(nanosecs(x) - nanosecs(p))
+    end
+end
