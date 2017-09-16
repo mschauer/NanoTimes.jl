c9378905f452442a9559b79b0d994499770ae68b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 02:53:17 -0400

    Update time_and_span.jl

--- src/types/mixed/time_and_span.jl
+++ src/types/mixed/time_and_span.jl
@@ -4,6 +4,7 @@ promote_type(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int12
 promote_rule(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int128}
 
 NanoTime{I2}(x::NanoTime{Int64}) where I2<:Int128 = NanoTime{I2}(value(x))
+convert(::Type{NanoTime{Int128}}, x::NanoTime{Int64}) = NanoTime{Int128}(x)
 
 function NanoTime{I1}(x::NanoTime{Int128}) where I1<:Int64
     if value(x) < typemin(Int64) || value(x) > typemax(Int64)
@@ -11,11 +12,13 @@ function NanoTime{I1}(x::NanoTime{Int128}) where I1<:Int64
     end
     return NanoTime{Int64}(value(x))
 end
+convert(::Type{NanoTime{Int64}}, x::NanoTime{Int128}) = NanoTime{Int64}(x)
 
 promote_type(::Type{NanoSpan{Int64}}, ::Type{NanoSpan{Int128}}) = NanoSpan{Int128}
 promote_rule(::Type{NanoSpan{Int64}}, ::Type{NanoSpan{Int128}}) = NanoSpan{Int128}
 
 NanoSpan{I2}(x::NanoSpan{Int64}) where I2<:Int128 = NanoSpan{Int128}(value(x))
+convert(::Type{NanoSpan{Int128}}, x::NanoSpan{Int64}) = NanoSpan{Int128}(x)
 
 function NanoSpan{I1}(x::NanoSpan{Int128}) where I1<:Int64
     if value(x) < typemin(Int64) || value(x) > typemax(Int64)
@@ -23,6 +26,7 @@ function NanoSpan{I1}(x::NanoSpan{Int128}) where I1<:Int64
     end
     return NanoSpan{Int64}(value(x))
 end
+convert(::Type{NanoSpan{Int64}}, x::NanoSpan{Int128}) = NanoSpan{Int64}(x)
 
 promote(x::NanoTime{Int64},  y::NanoSpan{Int128}) = (NanoTime{Int128}(x), y)
 promote(x::NanoTime{Int128}, y::NanoSpan{Int64})  = (x, NanoSpan{Int128}(y))
