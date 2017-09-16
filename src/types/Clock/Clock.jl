a2e74c7a03c5230fb33829ab7edfbe25c502c73e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 15:51:48 -0400

    Update Clock.jl

--- src/types/Clock/Clock.jl
+++ src/types/Clock/Clock.jl
@@ -3,3 +3,13 @@ struct Clock{T} <: AbstractClock
 end
 
 @inline value(x::Clock{T}) where T<:IntTimes = x.value
+
+Clock(value<:T) where T<:IntTimes = Clock{T}(value)
+
+function Clock(value<:T) where T
+    try
+         return convert(Clock, value)
+     catch e
+         throw(e)
+     end
+end
