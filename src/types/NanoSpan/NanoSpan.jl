ddba754baf6acdfa3c0211783d37f54ff35b5db2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 01:56:38 -0400

    Update NanoSpan.jl

--- src/types/NanoSpan/NanoSpan.jl
+++ src/types/NanoSpan/NanoSpan.jl
@@ -3,13 +3,3 @@ struct NanoSpan{T} <: AbstractNanoSpan
 end
 
 @inline value(x::NanoSpan{T}) where T<:IntTimes = x.value
- 
-Base.promote_type(::Type{NanoSpan{Int64}}, ::Type{NanoSpan{Int128}}) = NanoSpan{Int128}
-Base.promote_rule(::Type{NanoSpan{Int64}}, ::Type{NanoSpan{Int128}}) = NanoSpan{Int128}
-Base.convert(::Type{NanoSpan{Int128}}, x::NanoSpan{Int64}) = NanoSpan{Int128}(value(x))
-function Base.convert(::Type{NanoSpan{Int64}}, x::NanoSpan{Int128})
-    if value(x) < typemin(Int64) || value(x) > typemax(Int64)
-       throw(InexactError("x out of bounds $(typemin(Int64)) <= $(x) <= $(typemax(Int64))"))
-    end
-    return NanoSpan{Int64}(value(x))
-end
