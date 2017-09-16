0fe38044434ea48c91c53f65019ef74d9b0eb8b5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 01:52:27 -0400

    Update NanoSpan.jl

--- src/types/NanoSpan/NanoSpan.jl
+++ src/types/NanoSpan/NanoSpan.jl
@@ -14,4 +14,8 @@ function Base.convert(::Type{NanoSpan{Int64}}, x::NanoSpan{Int128})
     return NanoSpan{Int64}(value(x))
 end
 
+Base.promote(x::NanoTime{Int64},  y::NanoSpan{Int128}) = (NanoTime{Int128}(x), y)
+Base.promote(x::NanoTime{Int124}, y::NanoSpan{Int64})  = (x, NanoSpan{Int128}(y))
+Base.promote(x::NanoSpan{Int64},  y::NanoTime{Int128}) = (NanoSpan{Int128}(x), y)
+Base.promote(x::NanoSpan{Int128}, y::NanoTime{Int64})  = (x, NanoTime{Int128}(y))
 
