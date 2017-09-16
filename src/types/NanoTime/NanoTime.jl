b86bfa71b2af31d9e1127fbb01709d1ece443f97
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-16 01:48:37 -0400

    Update NanoTime.jl

--- src/types/NanoTime/NanoTime.jl
+++ src/types/NanoTime/NanoTime.jl
@@ -4,6 +4,7 @@ end
 
 @inline value(x::NanoTime{T}) where T<:IntTimes = x.value
 
+Base.promote_type(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int128}
 Base.promote_rule(::Type{NanoTime{Int64}}, ::Type{NanoTime{Int128}}) = NanoTime{Int128}
 Base.convert(::Type{NanoTime{Int128}}, x::NanoTime{Int64}) = NanoTime{Int128}(value(x))
 function Base.convert(::Type{NanoTime{Int64}}, x::NanoTime{Int128})
