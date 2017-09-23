d8e905cb48806d8cf3af8700f3c5902feff1b0c6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 21:07:50 -0400

    Update arith.jl

--- src/types/Span/arith.jl
+++ src/types/Span/arith.jl
@@ -2,22 +2,22 @@ import Base: (+), (-)
 
 function (+)(x::Span{I}, y::Span{I}) where I<:IntTimes
     nanos = nanoseconds(x) + nanoseconds(y)
-    return Span(nanos)
+    return Span{I}(nanos)
 end
 
 function (-)(x::Span{I}, y::Span{I}) where I<:IntTimes
     nanos = nanoseconds(x) - nanoseconds(y)
-    return Span(nanos)
+    return Span{I}(nanos)
 end
 
 (-)(x::Span{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
-(-)(x::Span{I}, y::Time) where I<:IntTimes = (-)(x, Span(y))
-(-)(x::DateTime, y::Span{I}) where I<:IntTimes = (-)(Clock(x), y)
-(-)(x::Date, y::Span{I}) where I<:IntTimes = (-)(Clock(x), y)
+(-)(x::Span{I}, y::Time) where I<:IntTimes = (-)(x, Span{I}(y))
+(-)(x::DateTime, y::Span{I}) where I<:IntTimes = (-)(Clock{I}(x), y)
+(-)(x::Date, y::Span{I}) where I<:IntTimes = (-)(Clock{I{(x), y)
 
 
-(+)(x::Span{I}, y::Time) where I<:IntTimes = (+)(x, Span(y))
-(+)(x::Time, y::Span{I}) where I<:IntTimes = (+)(y, Span(x))
+(+)(x::Span{I}, y::Time) where I<:IntTimes = (+)(x, Span{I}(y))
+(+)(x::Time, y::Span{I}) where I<:IntTimes = (+)(y, Span{I}(x))
 
 for P in (:Nanosecond, :Microsecond, :Millisecond, :Second, :Minute, :Hour, :Day, :Week)
     @eval begin
