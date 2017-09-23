3e90481de32f92c2d383ec87b51ad78076a4cae7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 21:08:25 -0400

    Update arith.jl

--- src/types/Clock/arith.jl
+++ src/types/Clock/arith.jl
@@ -2,18 +2,18 @@ import Base: (+), (-)
 
 function (-)(x::Clock{I}, y::Clock{I}) where I<:IntTimes
     nanos = nanoseconds(x) - nanoseconds(y)
-    return Span(nanos)
+    return Span{I}(nanos)
 end
 
 (-)(x::Clock{I1}, y::Clock{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
-(-)(x::Clock{I}, y::DateTime) where I<:IntTimes = (-)(x, Clock(y))
-(-)(x::DateTime, y::Clock{I}) where I<:IntTimes = (-)(Clock(x), y)
-(-)(x::Clock{I}, y::Date) where I<:IntTimes = (-)(x, Clock(y))
-(-)(x::Date, y::Clock{I}) where I<:IntTimes = (-)(Clock(x), y)
-(-)(x::Clock{I}, y::Time) where I<:IntTimes = (-)(x, Span(y))
+(-)(x::Clock{I}, y::DateTime) where I<:IntTimes = (-)(x, Clock{I}(y))
+(-)(x::DateTime, y::Clock{I}) where I<:IntTimes = (-)(Clock{I}(x), y)
+(-)(x::Clock{I}, y::Date) where I<:IntTimes = (-)(x, Clock{I}(y))
+(-)(x::Date, y::Clock{I}) where I<:IntTimes = (-)(Clock{I}(x), y)
+(-)(x::Clock{I}, y::Time) where I<:IntTimes = (-)(x, Span{I}(y))
 
-(+)(x::Clock{I}, y::Time) where I<:IntTimes = (+)(x, Span(y))
-(+)(x::Time, y::Clock{I}) where I<:IntTimes = (+)(y, Span(x))
+(+)(x::Clock{I}, y::Time) where I<:IntTimes = (+)(x, Span{I}(y))
+(+)(x::Time, y::Clock{I}) where I<:IntTimes = (+)(y, Span{I}(x))
 
 for P in (:Nanosecond, :Microsecond, :Millisecond, :Second, :Minute, :Hour, :Day, :Week)
     @eval begin
