a66568a7520efa6f6d9d4806aaa530bb25eabb9a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 20:53:15 -0400

    Update arith.jl

--- src/types/mixed/arith.jl
+++ src/types/mixed/arith.jl
@@ -7,4 +7,3 @@ import Base: (+), (-)
 
 (-)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock(value(x) - value(y))
 (-)(x::Clock{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
-(-)(x::Span{I}, y::Time) where I<:IntTimes = (-)(x, Span(y))
