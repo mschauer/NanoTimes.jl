c2d45c072c77e01288d4d144dab4a24f80b89de6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 20:46:06 -0400

    Update arith.jl

--- src/types/Span/arith.jl
+++ src/types/Span/arith.jl
@@ -6,6 +6,9 @@ function (-)(x::Span{I}, y::Span{I}) where I<:IntTimes
 end
 
 (-)(x::Span{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
+(-)(x::Span{I}, y::Time) where I<:IntTimes = (-)(x, Span(y))
+(-)(x::DateTime, y::Span{I}) where I<:IntTimes = (-)(Clock(x), y)
+(-)(x::Date, y::Span{I}) where I<:IntTimes = (-)(Clock(x), y)
 
 for P in (:Nanosecond, :Microsecond, :Millisecond, :Second, :Minute, :Hour, :Day, :Week)
     @eval begin
