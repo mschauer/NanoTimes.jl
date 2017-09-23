c61f146ebe25ca8f0846876cc2083b6bf6621692
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 21:23:06 -0400

    Update convert.jl

--- src/types/Clock/convert.jl
+++ src/types/Clock/convert.jl
@@ -1,40 +1,43 @@
-function Clock(dt::Date)
+function Clock{I}(dt::Date) where I<:IntTimes
     rata = date2rata(dt)
     rataguard(rata)
-    nanosec = (rata - RATAFIRST) * NANOSECONDS_PER_DAY
+    nanosec = I( (rata - RATAFIRST) * NANOSECONDS_PER_DAY )
     return Clock(nanosec)
 end
 
-function Date(x::Clock)
-    dat = RATAFIRST + fld(nanoseconds(x), NANOSECONDS_PER_DAY)
-    return rata2date(dat)
-end
-
-function Time(x::Clock)
-    return Time(Nanosecond(value(x)))
-end
-
-function Clock(dtm::DateTime)
+function Clock{I}(dtm::DateTime) where I<:IntTimes
     dat = Date(dtm)
     tim = Time(dtm)
-    return Clock(dat, tim)
+    return Clock{I}(dat, tim)
 end
 
-function DateTime(x::Clock)
-    dat, tim = fldmod(nanoseconds(x), NANOSECONDS_PER_DAY)
-    dat = rata2date(dat + RATAFIRST)
-    tim = Time(Nanosecond(tim))
-    return dat+tim
-end
-
-function Clock(dat::Date, tim::Time)
+function Clock{I}(dat::Date, tim::Time) where I<:IntTimes
     rata = date2rata(dat)
     rataguard(rata)
     nanosec = (rata - RATAFIRST) * NANOSECONDS_PER_DAY
     nanosec += tim.instant.value
-    return Clock(nanosec)
+    return Clock(I(nanosec))
 end
 
-function Clock(x::Time)
+function Clock{I}(x::Time) where I<:IntTimes
     error("use Span(x::Time), not Clock(x::Time)")
+end
+
+function Date(x::Clock{I}) where I<:IntTimes
+    nanos = round(Int64, fld(nanoseconds(x), NANOSECONDS_PER_DAY))
+    dat = RATAFIRST + nanos
+    return rata2date(dat)
+end
+
+function Time(x::Clock{I}) where I<:IntTimes
+    nanos = round(Int64, nanoseconds(x))
+    return Time(Nanosecond(nanos))
+end
+
+function DateTime(x::Clock)
+    nanos = round(Int64, nanoseconds(x))
+    dat, tim = fldmod(nanos, NANOSECONDS_PER_DAY)
+    dat = rata2date(dat + RATAFIRST)
+    tim = Time(Nanosecond(tim))
+    return dat+tim
 end
