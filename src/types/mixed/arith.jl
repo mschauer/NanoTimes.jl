6ae42906af5185d78804d3fda88d8182aaeffa46
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 20:58:05 -0400

    Update arith.jl

--- src/types/mixed/arith.jl
+++ src/types/mixed/arith.jl
@@ -8,4 +8,7 @@ import Base: (+), (-)
 (-)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock(value(x) - value(y))
 (-)(x::Clock{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
 
-(-)(x::Span{I}, y::Clock{I}) where I<:IntTimes = throw(ErrorException("Span-Clock is invalid;  write Clock-Span."))
+(-)(x::Span{I}, y::Clock{I}) where I<:IntTimes =
+    throw(ErrorException("Span-Clock is invalid;  write Clock-Span."))
+(-)(x::Span{I1}, y::Clock{I2}) where {I1<:IntTimes, I2<:IntTimes} =
+    throw(ErrorException("Span-Clock is invalid;  write Clock-Span."))
