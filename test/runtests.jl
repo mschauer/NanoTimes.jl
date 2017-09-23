9c309dfa1def12ba9581af560c04ef08b151f329
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-23 15:20:45 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,18 +6,18 @@ date1 = Date("1995-02-17")
 date2 = Date("2012-04-18")
 datm1 = DateTime("1995-02-17T03:22:17.012")
 datm2 = DateTime("2012-04-18T17:13:08")
-time1 = Base.Dates.Time(datm1)
-time2 = Base.Dates.Time(datm2)
+time1 = Time24(datm1)
+time2 = Time24(datm2)
 
 ddate = date2 - date1
 ddatm = datm2 - datm1
 dtime = time2 - time1
 
-ndate1 = Clock(date1)
-ndate2 = Clock(date2)
-ndatm1 = Clock(datm1)
-ndatm2 = Clock(datm2)
-ndatm3 = Clock(datm2) - Day(12) + Milliseconds(98765) - Microsecond(123) - Nanosecond(2)
+ndate1 = Time(date1)
+ndate2 = Time(date2)
+ndatm1 = Time(datm1)
+ndatm2 = Time(datm2)
+ndatm3 = Time(datm2) - Day(12) + Milliseconds(98765) - Microsecond(123) - Nanosecond(2)
 nspan1 = Span(time1)
 nspan2 = Span(time2)
 
@@ -32,8 +32,8 @@ dnspan = nspan2 - nspan1
 @test dndate == Span(ddate)
 @test dnspan == Span(time2 - time1)
 
-@test ndate1 == parse(Clock, string(ndate1))
-@test ndatm2 == parse(Clock, string(ndatm2))
+@test ndate1 == parse(Time, string(ndate1))
+@test ndatm2 == parse(Time, string(ndatm2))
 @test nspan1 == parse(Span, string(nspan1))
 @test -dnspan == -parse(Span, string(dnspan))
 
