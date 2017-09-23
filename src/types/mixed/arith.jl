92cad192fa3eacb9bcb570bbf07da936d5f87e4e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 22:24:49 -0400

    Update arith.jl

--- src/types/mixed/arith.jl
+++ src/types/mixed/arith.jl
@@ -1,11 +1,11 @@
 import Base: (+), (-)
 
-(+)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock{I}(nanosecs(x) + nanosecs(y))
-(+)(x::Span{I}, y::Clock{I}) where I<:IntTimes = Clock{I}(nanosecs(x) + nanosecs(y))
+(+)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock{I}(nanoseconds(x) + nanoseconds(y))
+(+)(x::Span{I}, y::Clock{I}) where I<:IntTimes = Clock{I}(nanoseconds(x) + nanoseconds(y))
 (+)(x::Clock{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (+)(promote(x, y)...)
 (+)(x::Span{I1}, y::Clock{I2}) where {I1<:IntTimes, I2<:IntTimes} = (+)(promote(x, y)...)
 
-(-)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock{I}(nanosecs(x) - nanosecs(y))
+(-)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock{I}(nanoseconds(x) - nanoseconds(y))
 (-)(x::Clock{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
 
 (-)(x::Span{I}, y::Clock{I}) where I<:IntTimes =
