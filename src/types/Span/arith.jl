c4794e4f3563549b66c7c95f8bd32c4814583d66
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 21:01:35 -0400

    Update arith.jl

--- src/types/Span/arith.jl
+++ src/types/Span/arith.jl
@@ -10,6 +10,10 @@ end
 (-)(x::DateTime, y::Span{I}) where I<:IntTimes = (-)(Clock(x), y)
 (-)(x::Date, y::Span{I}) where I<:IntTimes = (-)(Clock(x), y)
 
+
+(+)(x::Span{I}, y::Time) where I<:IntTimes = (+)(x, Span(y))
+(+)(x::Time, y::Span{I}) where I<:IntTimes = (+)(y, Span(x))
+
 for P in (:Nanosecond, :Microsecond, :Millisecond, :Second, :Minute, :Hour, :Day, :Week)
     @eval begin
         (+)(x::Span{I}, p::$P) where I<:IntTimes = Span{I}(nanosecs(x) + nanosecs(p))
