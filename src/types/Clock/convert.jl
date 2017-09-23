b66d519781db4120a6d73075bb4f67224013f62c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-23 14:30:24 -0400

    Update convert.jl

--- src/types/Clock/convert.jl
+++ src/types/Clock/convert.jl
@@ -39,13 +39,13 @@ end
 
 function Base.Dates.Time(x::Clock{I}) where I<:IntTimes
     nanos = round(Int64, nanoseconds(x))
-    return Time(Nanosecond(nanos))
+    return Base.Dates.Time(Nanosecond(nanos))
 end
 
 function DateTime(x::Clock{I}) where I<:IntTimes
     nanos = round(Int64, nanoseconds(x))
     dat, tim = fldmod(nanos, NANOSECONDS_PER_DAY)
     dat = rata2date(dat + RATAFIRST)
-    tim = Time(Nanosecond(tim))
+    tim = Base.Dates.Time(Nanosecond(tim))
     return dat+tim
 end
