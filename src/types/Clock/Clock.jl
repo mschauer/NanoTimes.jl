4df33c00064f254285b2bd74bd66efbc83199db6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 20:10:36 -0400

    Update Clock.jl

--- src/types/Clock/Clock.jl
+++ src/types/Clock/Clock.jl
@@ -4,7 +4,7 @@ end
 
 @inline value(x::Clock{T}) where T<:IntTimes = x.value
 
-Clock(value<:T) where T<:IntTimes = Clock{T}(value)
+Clock(value::T) where T<:IntTimes = Clock{T}(value)
 
 function Clock(value<:T) where T
     try
