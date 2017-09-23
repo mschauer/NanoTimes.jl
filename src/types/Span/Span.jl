ef13ff1edda3a7990a5c306ac12b0e7a719442d5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 20:25:19 -0400

    Update Span.jl

--- src/types/Span/Span.jl
+++ src/types/Span/Span.jl
@@ -1,8 +1,12 @@
 struct Span{T} <: AbstractSpan{T}
     value::T
+
+    function Span(value::T) where T<:IntTimes
+        return new{T}(value)
+    end
 end
 
-@inline value(x::Span{T}) where T<:IntTimes = x.value
+#@inline value(x::Span{T}) where T<:IntTimes = x.value
 
 Span(value::T) where T<:IntTimes = Span{T}(value)
 
