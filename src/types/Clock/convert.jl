7ee54767a30c6d240c1f326591c6ff56e996fc2f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 21:33:01 -0400

    Update convert.jl

--- src/types/Clock/convert.jl
+++ src/types/Clock/convert.jl
@@ -5,11 +5,15 @@ function Clock{I}(dt::Date) where I<:IntTimes
     return Clock(nanosec)
 end
 
+@inline Clock(dt::Date) = Clock{Int64}(dt)
+
 function Clock{I}(dtm::DateTime) where I<:IntTimes
     dat = Date(dtm)
     tim = Time(dtm)
     return Clock{I}(dat, tim)
 end
+
+@inline Clock(dtm::DateTime) = Clock{Int64}(dtm)
 
 function Clock{I}(dat::Date, tim::Time) where I<:IntTimes
     rata = date2rata(dat)
@@ -18,6 +22,9 @@ function Clock{I}(dat::Date, tim::Time) where I<:IntTimes
     nanosec += tim.instant.value
     return Clock(I(nanosec))
 end
+
+@inline Clock(dat::Date, tim::Time) = Clock{Int64}(dat, tim)
+
 
 function Clock{I}(x::Time) where I<:IntTimes
     error("use Span(x::Time), not Clock(x::Time)")
@@ -34,7 +41,7 @@ function Time(x::Clock{I}) where I<:IntTimes
     return Time(Nanosecond(nanos))
 end
 
-function DateTime(x::Clock)
+function DateTime(x::Clock{I}) where I<:IntTimes
     nanos = round(Int64, nanoseconds(x))
     dat, tim = fldmod(nanos, NANOSECONDS_PER_DAY)
     dat = rata2date(dat + RATAFIRST)
