c7e2a7802ff2a343a1e12f8072ffbd3354a134dc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 20:10:59 -0400

    Update Span.jl

--- src/types/Span/Span.jl
+++ src/types/Span/Span.jl
@@ -4,7 +4,7 @@ end
 
 @inline value(x::Span{T}) where T<:IntTimes = x.value
 
-Span(value<:T) where T<:IntTimes = Span{T}(value)
+Span(value::T) where T<:IntTimes = Span{T}(value)
 
 function Span(value<:T) where T
     try
