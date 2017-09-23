01165c686861c61de95fa851e462d5214ccf17e1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 21:55:48 -0400

    Create periods.jl

---
+++ src/types/mixed/periods.jl
@@ -0,0 +1,11 @@
+@inline nanoseconds(x::Day)  = Base.Dates.value(x) * NANOSECONDS_PER_DAY
+@inline nanoseconds(x::Hour) = Base.Dates.value(x) * NANOSECONDS_PER_HOUR
+@inline nanoseconds(x::Minute) = Base.Dates.value(x) * NANOSECONDS_PER_MINUTE
+@inline nanoseconds(x::Second) = Base.Dates.value(x) * NANOSECONDS_PER_SECOND
+@inline nanoseconds(x::Millisecond) = Base.Dates.value(x) * NANOSECONDS_PER_MILLISEC
+@inline nanoseconds(x::Microsecond) = Base.Dates.value(x) * NANOSECONDS_PER_MICROSEC
+@inline nanoseconds(x::Nanosecond)  = Base.Dates.value(x)
+
+for P in (:Day, :Hour, :Minute, :Second, :Millisecond, :Microsecond, :Nanosecond)
+    @eval Nanoseconds(x::$P) = Nanoseconds(nanoseconds(x))
+end
