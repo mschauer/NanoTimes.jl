97b4a61230a69179e091a8ecae13d1f4ac672ce9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 22:44:03 -0400

    Update parse.jl

--- src/types/Span/parse.jl
+++ src/types/Span/parse.jl
@@ -2,6 +2,13 @@ const ZEROHOUR = parse(Time, "00:00:00")
 
 function Base.parse(::Type{Span}, str::String)
     str = strip(str)
+    isneg = str[1] === NEGATIVE || str[1] === '-'
+    ispos = str[1] == POSITIVE
+    if isneg || ispos
+        str = str[2:end]
+    else
+        ispos = true
+    end
     n = length(str)
     if contains(str, 'T')
         strs = split(str,'T')
@@ -11,10 +18,12 @@ function Base.parse(::Type{Span}, str::String)
         strs = [string("20",str[1:2],'-',str[3:4],'-',str[5:6]), ""]
     elseif n == 10
         strs = [str, ""]
+    elseif str[3] == COLON
+        strs = ["", str]
     else
         throw(ErrorException("$(str) is not recognized as a Timespan value"))
     end
-    date = parse(Date, strs[1])
+    date = strs[1] != "" : parse(Date, strs[1]) : 0
     if contains(strs[2],'.')
        strs = split(strs[2], '.')
        time = parse(Base.Dates.Time, strs[1])
@@ -24,5 +33,6 @@ function Base.parse(::Type{Span}, str::String)
     else
        time = parse(Base.Dates.Time,strs[2])
     end
-    return Span(date, time)
+    result = date==0 ? Span(time) : Span(date, time)
+    return isneg ? -result : result
 end
