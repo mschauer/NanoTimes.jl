169d81ee681afc65c3cd7601dd8445c7584c3b83
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-23 14:35:20 -0400

    Update string.jl

--- src/types/Clock/string.jl
+++ src/types/Clock/string.jl
@@ -1,18 +1,18 @@
 function Base.string(x::Clock{T}) where T
     dt = Date(x)
-    tm = Time(x)
+    tm = Base.Dates.Time(x)
     return string(dt, "T", tm)
 end
 
 function Base.string(x::Clock{T}, suffix::Char) where T
     dt = Date(x)
-    tm = Time(x)
+    tm = Base.Dates.Time(x)
     return string(dt, "T", tm, suffix)
 end
 
 function Base.string(x::Clock{T}, lcl2ut::Second) where T
     dt = Date(x)
-    tm = Time(x)
+    tm = Base.Dates.Time(x)
     mins = div(lcl2ut.value, SECONDS_PER_MINUTE)
     offset = signed_hoursmins(mins)
     return string(dt, "T", tm, offset)
