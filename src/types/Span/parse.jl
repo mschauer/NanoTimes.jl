16b2a9ed49d5b40d1acc4a2ac531a944a47acd2a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 20:18:02 -0400

    Update parse.jl

--- src/types/Span/parse.jl
+++ src/types/Span/parse.jl
@@ -7,9 +7,9 @@ function Base.parse(::Type{Span}, str::String)
         strs = split(str,'T')
     elseif contains(str, ' ')
         strs = split(str,' ')
-    elseif n=6
+    elseif n == 6
         strs = [string("20",str[1:2],'-',str[3:4],'-',str[5:6]), ""]
-    elseif n=10
+    elseif n == 10
         strs = [str, ""]
     else
         throw ErrorException("$(str) is not recognized as a Date[+TimeOfDay] value")
@@ -19,7 +19,7 @@ function Base.parse(::Type{Span}, str::String)
        strs = split(strs[2], '.')
        time = parse(Base.Dates.Time, strs[1]
        time += Base.Dates.Nanosecond(parse(Int64, strs[2])
-    elseif length(strs[2] == 0
+    elseif length(strs[2]) == 0
        time = ZEROHOUR
     else
        time = parse(Base.Dates.Time,strs[2])
