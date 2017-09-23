28f23ee4744954188390c3841d80fca61c70c4a7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-22 21:39:17 -0400

    Update convert.jl

--- src/types/Span/convert.jl
+++ src/types/Span/convert.jl
@@ -2,18 +2,39 @@ function Span{I}(x::Time) where I<:IntTimes
     return Span(I(x.instant.value))
 end
 
+@inline Span(x::Time) = Span{Int64}(dt)
+
 function Span{I}(x::CompoundPeriod) where I<:IntTimes
     nanosecs = I(sum(map(a->Nanosecond(a), x.periods)))
     return Span(nanosecs.value)
 end
 
-Span(x::Day)  = Span(x.value * NANOSECONDS_PER_DAY)
-Span(x::Hour) = Span(x.value * NANOSECONDS_PER_HOUR)
-Span(x::Minute) = Span(x.value * NANOSECONDS_PER_MINUTE)
-Span(x::Second) = Span(x.value * NANOSECONDS_PER_SECOND)
-Span(x::Millisecond) = Span(x.value * NANOSECONDS_PER_MILLISEC)
-Span(x::Microsecond) = Span(x.value * NANOSECONDS_PER_MICROSEC)
-Span(x::Nanosecond)  = Span(x.value)
+@inline Span(x::CompoundPeriod) = Span{Int64}(x)
+
+Span(x::Day)  = Span{Int64}(x.value * NANOSECONDS_PER_DAY)
+Span(x::Hour) = Span{Int64}(x.value * NANOSECONDS_PER_HOUR)
+Span(x::Minute) = Span{Int64}(x.value * NANOSECONDS_PER_MINUTE)
+Span(x::Second) = Span{Int64}(x.value * NANOSECONDS_PER_SECOND)
+Span(x::Millisecond) = Span{Int64}(x.value * NANOSECONDS_PER_MILLISEC)
+Span(x::Microsecond) = Span{Int64}(x.value * NANOSECONDS_PER_MICROSEC)
+Span(x::Nanosecond)  = Span{Int64}(x.value)
+
+Span{Int64}(x::Day)  = Span{Int64}(x.value * NANOSECONDS_PER_DAY)
+Span{Int64}(x::Hour) = Span{Int64}(x.value * NANOSECONDS_PER_HOUR)
+Span{Int64}(x::Minute) = Span{Int64}(x.value * NANOSECONDS_PER_MINUTE)
+Span{Int64}(x::Second) = Span{Int64}(x.value * NANOSECONDS_PER_SECOND)
+Span{Int64}(x::Millisecond) = Span{Int64}(x.value * NANOSECONDS_PER_MILLISEC)
+Span{Int64}(x::Microsecond) = Span{Int64}(x.value * NANOSECONDS_PER_MICROSEC)
+Span{Int64}(x::Nanosecond)  = Span{Int64}(x.value)
+
+Span{Int128}(x::Day)  = Span(Int128(x.value * NANOSECONDS_PER_DAY))
+Span{Int128}(x::Hour) = Span(Int128(x.value * NANOSECONDS_PER_HOUR))
+Span{Int128}(x::Minute) = Span(Int128(x.value * NANOSECONDS_PER_MINUTE))
+Span{Int128}(x::Second) = Span(Int128(x.value * NANOSECONDS_PER_SECOND))
+Span{Int128}(x::Millisecond) = Span{Int128(x.value * NANOSECONDS_PER_MILLISEC))
+Span{Int128}(x::Microsecond) = Span(Int128(x.value * NANOSECONDS_PER_MICROSEC))
+Span{Int128}(x::Nanosecond)  = Span(Int128(x.value))
+
 
 function Span(; days::Int64=0,
                     hours::Int64=0,
