be7113fe822849d576cb01d7f653354f9d08cf27
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-23 22:16:00 -0400

    Update periods.jl

--- src/types/mixed/periods.jl
+++ src/types/mixed/periods.jl
@@ -9,3 +9,14 @@
 for P in (:Day, :Hour, :Minute, :Second, :Millisecond, :Microsecond, :Nanosecond)
     @eval Nanoseconds(x::$P) = Nanoseconds(nanoseconds(x))
 end
+
+Base.Dates.year(x::Base.Dates.Year) = x.value
+Base.Dates.month(x::Base.Dates.Month) = x.value
+Base.Dates.week(x::Base.Dates.Week) = x.value
+Base.Dates.day(x::Base.Dates.Day) = x.value
+Base.Dates.hour(x::Base.Dates.Hour) = x.value
+Base.Dates.minute(x::Base.Dates.Minute) = x.value
+Base.Dates.second(x::Base.Dates.Second) = x.value
+Base.Dates.millisecond(x::Base.Dates.Millisecond) = x.value
+Base.Dates.microsecond(x::Base.Dates.Microsecond) = x.value
+Base.Dates.nanosecond(x::Base.Dates.Nanosecond) = x.value
