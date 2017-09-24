2b48dfd7198b4ad716e92b84637e01eb6050b167
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-24 00:32:31 -0400

    Update Abstract.jl

--- src/types/Abstract.jl
+++ src/types/Abstract.jl
@@ -1,26 +1,70 @@
+
+abstract type SpatioTemporal end
+
+abstract type Spatial  <: SpatioTemporal end
+abstract type Temporal <: SpatioTemporal end
+
+#=>
+    Type Parameters
+
+    T is the primitive type used for time keeping in the applicable frame
+    Z is locative as an operational relative frame for temporal reference
+<=#
+
+abstract type AbstractMoment{T}    <: Temporal end
+abstract type AbstractDuration{T}  <: Temporal end
+
+#=>
+    Simple Types
+
+    These types are free of determinative location and live as proleptic Gregorians
+<=#
+
+abstract type SimpleClocks{T}      <: AbstractMoment{T}   end  # date with timeofday
+                                                               # moments    are the *tocks*
+                                                               # timestamps are the *ticks*
+
+abstract type SimpleDates{T}       <: SimpleClocks{T}     end  # date without timeofday
+abstract type SimpleTimesOfDay{T}  <: SimpleClocks{T}     end  # timeofday without date
+
+#=>
+    Informed Types
+
+    These types allow temporal frameworks and admit location-aware zones
+<=#
+
+abstract type Clocks{T,Z}     <: SimpleClocks{T}     end  # date with timeofday
+abstract type Dates{T,Z}      <: SimpleDates{T}      end  # date without timeofday
+abstract type TimesOfDay{T,Z} <: SimpleTimesOfDay{T} end  # timeofday without date
+
+#=>
+    Kinds of Duration
+
+    unattached ( not rooted to a specific initial moment )
+               *  clock2 - clock1, date2 - date1
+               *  three months and five days
+
+    attached   ( temporal exent taken from a specific initial moment )
+               *  the second Friday after the first Monday in September 2017
+               *  every other Tuesday starting with 2017-10-03
+               *  the end of the current Quarter
+
+    undirected ( temporal extent that is unsigned )
+               * a cover encompassing moments given as equi-grain [multi-]sequence[s]
+
+    oriented   ( temporal extent signed, and {- abs} apply )
+               * a span, an inclusion of moment-grain sequence[s]
+<=#
+
+abstract type AbstractTimePeriod{T} <: AbstractDuration{T} end # attachable undirected
+abstract type AbstractTimeSpan{T}   <: AbstractDuration{T} end # unattached oriented
+
+abstract type TimePeriods{T,Z} <: AbstractTimePeriod{T} end # Z relative, mean solar seconds
+abstract type TimeSpans{T,Z}   <: AbstractTimeSpan{T}   end # Z relative, 86400 SI seconds
+
+#= was
 import Base.Dates.AbstractTime
 
 abstract type AbstractTimeDate{T} <: AbstractTime end
 abstract type AbstractTimeSpan{T} <: AbstractTime end
-
-#=
-const IntsForTime = Union{Int64, Int128}
-
-struct TimeDate{T} <: AbstractTimeDate{T}
-    value::T
-
-    function TimeDate(value::T) where T<:IntsForTime
-        return TimeDate{T}(value)
-    end
-end
-
-struct TimeSpan{T} <: AbstractTimeSpan{T}
-    value::T
-
-    function TimeSpan(value::T) where T<:IntsForTime
-        return TimeSpan{T}(value)
-    end
-end
-
 =#
-
